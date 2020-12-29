edd76eddc1621bc28878ac8ecc2dbfaccff177af
hyeonjeong lee <hlee220@myseneca.ca>
2020-12-30 01:18:45 +0900

    태그수정

--- src/components/Projects/Hyolo.jsx
+++ src/components/Projects/Hyolo.jsx
@@ -1,86 +1,64 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import Fade from 'react-reveal/Fade';
-import { Container, Row, Col } from 'react-bootstrap';
-import Title from '../Title/Title';
-import AboutImg from '../Image/AboutImg';
-import PortfolioContext from '../../context/context';
+import { Container } from 'react-bootstrap';
 
-const About = () => {
-  const { about } = useContext(PortfolioContext);
-  const { img, resume, blog } = about;
+import Title from  '../Title/Title';
+import imgfile from '../../images/main1.jpg';
 
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true);
-      setIsMobile(false);
-    } else {
-      setIsMobile(true);
-      setIsDesktop(false);
-    }
-  }, []);
 
+import 'swiper/swiper-bundle.css';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+const Hyolo = () => {
+    
   return (
-    <section id="about">
+    <section id="hyolo">
       <Container>
-        <Title title="About Me" />
-        <Row className="about-wrapper">
-          <Col md={6} sm={12}>
-            <Fade bottom duration={1000} delay={600} distance="30px">
-              <div className="about-wrapper__image">
-                <AboutImg alt="profile picture" filename={img} />
-              </div>
-            </Fade>
-          </Col>
-          <Col md={6} sm={12}>
-            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-              <div className="about-wrapper__info">
-                <p className="about-wrapper__info-text">안녕하세요. 신입 웹 개발자 이현정입니다!</p>
-                <p className="about-wrapper__info-text">
-                  오늘 보다 더 나은 내일의 Web Developer가 되기 위해 공부하는 주니어 웹 개발자
-                  입니다. 시작이 반이라는 말처럼 좋은 시작 역시 중요한 부분 중에 하나라고
-                  생각합니다.
-                </p>
-                <p className="about-wrapper__info-text">
-                  하지만 그 시작을 실천하는 것보다 꾸준하게 끝까지 해내는 것이 더 어렵다고 합니다.
-                  이렇듯 더 발전된 내일의 개발자가 되기 위해 꾸준한 모습을 보여드리겠습니다
-                </p>
-                <p className="about-wrapper__info-text">
-                  공부내용을 지속적으로 블로그에 업데이트 중입니다
-                </p>
-                <div>
-                  {resume && (
-                    <span>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--resume"
-                        href={resume}
-                      >
-                        Resume
-                      </a>
-                    </span>
-                  )}
-                  <span className="ml-3">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--resume"
-                      href={blog}
-                    >
-                      Blog
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </Fade>
-          </Col>
-        </Row>
+                 <Title title="HYOLO" />
+                 <Fade bottom duration={1000} delay={1200} distance="0px">
+                 <p>핫플레이스 예약 관리 사이트</p>
+                 <p>JSP/Servlet을 기반으로 전반적인 웹사이트의 서버 연결과 View를 개발하였고 일부 비동기 통신이 필요한 파트는 Ajax를 사용하여 구현하였습니다.<br/>
+                      데이터베이스는 JDBC를 사용하여 오라클을 연동하였습니다.<br />클라이언트 페이지의 대부분은 JavaScript를 사용하였고 디자인과 반응형 웹 사이트를 위해 Bootstrap을 사용하였습니다.</p>
+                      </Fade>
+        <Fade bottom duration={1000} delay={800} distance="30px">
+                      <div className="contact-wrapper">
+                           <Swiper
+                              spaceBetween={50}
+                              slidesPerView={1}
+                              navigation
+                              pagination={{ clickable: true }}
+                              scrollbar={{ draggable: true }}
+                         >
+                                <SwiperSlide>
+                                     <div id="slide1">
+                                          <p><img src={imgfile} class="hyolo-img" /></p>
+                                          <div class="img-text">
+                                               <p>캘린더 선택창은 Datepicker를 사용하여 구현 하였습니다.</p>
+                                        
+                                               <p>검색 기능은 POST 방식으로 form을 전달하여 검색기능을 구현하였습니다.
+                                               업체 회원이 예약 날짜를 설정해둔 업체만 추천 항목이 뜨도록 구현하였습니다.</p>
+                                          </div>
+                                          
+                                     </div>
+                                </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      
+    </Swiper>
+                           
+          </div>
+        </Fade>
       </Container>
     </section>
   );
 };
 
-export default About;
+export default Hyolo;
